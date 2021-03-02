ada3c396e963fc6b382353f1185762abb9d1f904
Mahmoud <mamdy@gmail.com>
2021-03-02 14:54:56 +0100

    userProfile

---
+++ src/main/java/com/mamdy/form/UpdatedUserForm.java
@@ -0,0 +1,18 @@
+package com.mamdy.form;
+
+import lombok.Data;
+
+@Data
+public class UpdatedUserForm {
+    private String civilite;
+    private String email;
+    private String lastName;
+    private String firstName;
+    private String password;
+    private String confirmedPassword;
+    private String phone;
+    private String address;
+    private String codePostal;
+    private String ville;
+    private String pays;
+}
