a5a693c5cd3b8959586a59ad1dc8e7566dc45ec6
Mahmoud <mamdy@gmail.com>
2021-05-15 14:53:51 +0200

    dbconfig

--- src/main/java/com/mamdy/DBaseConfig.java
+++ src/main/java/com/mamdy/DBaseConfig.java
@@ -13,7 +13,7 @@ public class DBaseConfig {
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
-    @Value("${spring.datasource.driver-class-name}")
+    @Value("${spring.datasource.driverClassName}")
     private String driverClassName;
 
 
@@ -25,8 +25,8 @@ public class DBaseConfig {
     public DataSource dataSource(){
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
-        config.setUsername("b53ad8c1e12d5f");
-        config.setPassword("bd43bb8d");
+        /*config.setUsername("b53ad8c1e12d5f");
+        config.setPassword("bd43bb8d");*/
         config.setDriverClassName(driverClassName);
         return new HikariDataSource(config);
     }
